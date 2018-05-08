d264d45c38c72574f4851741297d8637f19ca28a
frankamente <fran29400@gmail.com>
2018-05-08 13:25:15 +0200

    Tutorial3

---
+++ Tutorial3 Publish-Subscribe/Producer/producer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import pika
+import sys
+
+url = 'amqp://lvfqbbym:hZBmLLY4OVCWRbIHjXpKPr9XUNTcufcz@clam.rmq.cloudamqp.com/lvfqbbym'
+params = pika.URLParameters(url)
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+
+channel.exchange_declare(exchange='logs',
+                         exchange_type='fanout')
+
+message = ' '.join(sys.argv[1:]) or "info: Hello World!"
+channel.basic_publish(exchange='logs',
+                      routing_key='',
+                      body=message)
+print(" [x] Sent %r" % message)
+connection.close()
